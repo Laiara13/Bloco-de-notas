c96da9ffb2c47847022a89c6663aa3b4e8a4b65f
Laiara13 <laiara-leizer@educar.rs.gov.br>
2024-03-08 16:13:52 -0300

    versao 3

--- js/script.js
+++ js/script.js
@@ -4,5 +4,31 @@
 
 ************************************************/
 
-const addNote = document.querySelector("#add-note");
+let addNote = document.querySelector('#add-note');//Botão de para adicionar nota
+let closeModal =  document.querySelector('#close-modal'); //fechar janela modal com os detalhes da nota.
+let modal = document.querySelector('#modal'); //Modal para edição das notas
+let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
+let notes = document.querySelectorAll('.item-note');//Lista divs com dados das notas
+let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
+let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
 
+addNote.addEventListener('click',
+(evt)=>{
+  evt.preventDefault();
+  modal.style.display = 'block';
+  notes.style.display = 'none';
+  addNote.style.display = 'none';
+});
+
+
+btnCloseNote.addEventListener('click',
+(evt)=>{
+  evt.preventDefault();
+  modal.style.display = 'none';
+  notes.style.display = 'flex';
+  addNote.style.display = 'block';
+});
+
+btnCloseNote.addEventListener("click", (evt)=>{
+
+});
