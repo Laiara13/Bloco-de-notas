bfdccc54c3d62eba11714f74260b1c5b02f874a0
Laiara13 <laiara-leizer@educar.rs.gov.br>
2024-03-08 16:44:34 -0300

    versao 4

--- js/script.js
+++ js/script.js
@@ -12,6 +12,12 @@ let notes = document.querySelectorAll('.item-note');//Lista divs com dados das n
 let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
 let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
 
+/************************************************
+
+=====================EVENTOS=====================
+
+************************************************/
+
 addNote.addEventListener('click',
 (evt)=>{
   evt.preventDefault();
@@ -20,15 +26,36 @@ addNote.addEventListener('click',
   addNote.style.display = 'none';
 });
 
-
-btnCloseNote.addEventListener('click',
-(evt)=>{
+btnCloseNote.addEventListener('click', (evt)=>{
   evt.preventDefault();
   modal.style.display = 'none';
   notes.style.display = 'flex';
   addNote.style.display = 'block';
 });
 
-btnCloseNote.addEventListener("click", (evt)=>{
+btnSaveNote.addEventListener("click", (evt)=>{
+    evt.proventDefault();
 
+    let data = {
+        id: document.querySelector("#input-id").value,
+        title: document.querySelector("#input-title").value,
+        content: document.querySelector("#input-content").value,
+        lastTime: new Date().getTime(),
+    }
 });
+
+/************************************************
+
+=====================FUNÇÕES=====================
+
+************************************************/
+
+const saveNote = (data) => {
+    data = JSON.stringify(data);
+
+    console.log(data);
+    localStorage.getItem('notes');//se não tiver notes ele retorna um valor nulo
+
+    localStorage.setItem('notes', data);
+
+};
